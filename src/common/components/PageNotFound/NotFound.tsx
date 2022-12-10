f0f6d112efc1f1881b94b0fe044b5a79609f594a
grigorydobrenko <grigorydobrenko97@gmail.com>
2022-12-10 17:16:15 +0300

    feat: add material ui, add formik

--- src/common/components/PageNotFound/NotFound.tsx
+++ src/common/components/PageNotFound/NotFound.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import { Button } from '@mui/material'
+
 export const NotFound = () => {
-  return <div>not found</div>
+  return (
+    <div>
+      not found
+      <Button variant="text">Text</Button>
+    </div>
+  )
 }
