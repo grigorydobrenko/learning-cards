4a1ebffe34ad3ce726a612f7c5ccf1106b1292f3
grigorydobrenko <grigorydobrenko97@gmail.com>
2022-12-10 20:07:38 +0300

    feat: add login form

--- src/features/auth/Login/Login.tsx
+++ src/features/auth/Login/Login.tsx
@@ -1,5 +1,39 @@
 import React from 'react'
 
+import { Button, Checkbox, FormControlLabel, TextField } from '@mui/material'
+import FormControl from '@mui/material/FormControl'
+import FormGroup from '@mui/material/FormGroup'
+import { Link, Route, Routes } from 'react-router-dom'
+
+import { PATH } from '../../../common/components/Routing/Routes'
+import styles from '../../Header/Header.module.css'
+
 export const Login = () => {
-  return <div>Login</div>
+  return (
+    <div>
+      <h2>Sign in</h2>
+      <form>
+        <FormControl>
+          <FormGroup>
+            <TextField id="standard-basic" label="Email" variant="standard" />
+            <TextField id="standard-basic" label="Password" variant="standard" />
+            <FormControlLabel label={'Remember me'} control={<Checkbox />} />
+
+            <Link to={PATH.FORGOT_PASSWORD} className={styles.link}>
+              Forgot Password?
+            </Link>
+
+            <Button variant="contained">Sign In</Button>
+
+            <div>Already have an account?</div>
+            <Button variant="text">
+              <Link to={PATH.REGISTER} className={styles.link}>
+                Sign Up
+              </Link>
+            </Button>
+          </FormGroup>
+        </FormControl>
+      </form>
+    </div>
+  )
 }
