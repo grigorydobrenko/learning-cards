dd70dcd7fedc2f6f94bf284fbc138cf86204511e
Andrey Kulish <andreykulish89@gmail.com>
2022-12-13 17:26:30 +0300

    feat: add LinearProgress into App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 import './App.css'
 import { AppRoutes } from '../common/components/Routing/Routes'
 import { Header } from '../features/Header/Header'
+import LinearProgress from '@mui/material/LinearProgress'
 
 function App() {
   return (
     <div className="App">
+      <LinearProgress sx={{ position: 'absolute', width: '100%', height: '5px', top: '0' }} />
       <Header />
+
       <div className="routes">
         <AppRoutes />
       </div>
