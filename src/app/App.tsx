6efd67ef1bb09ed16aea7e11b9ea05ce44bba173
grigorydobrenko <grigorydobrenko97@gmail.com>
2022-12-13 18:23:42 +0300

    fix: fix padding top

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 import './App.css'
-import LinearProgress from '@mui/material/LinearProgress'
 import { Container } from '@mui/material'
+import LinearProgress from '@mui/material/LinearProgress'
 
 import { AppRoutes } from '../common/components/Routing/Routes'
 import { Header } from '../features/Header/Header'
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <LinearProgress sx={{ position: 'absolute', width: '100%', height: '5px', top: '0' }} />
       <Header />
-      <Container fixed>
+      <Container fixed sx={{ pt: 4 }}>
         <AppRoutes />
       </Container>
     </div>
