3d9bac288800f0b1a225f407e1eb2b9fb0fb8097
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-14 22:06:09 +0100

    feat: create isRegisteredIn for redirect to login after registration

--- src/features/auth/auth-reducer.ts
+++ src/features/auth/auth-reducer.ts
@@ -2,14 +2,27 @@ import { AppThunkDispatch, AppThunkType } from '../../app/store'
 
 import { registrationAPI, RegistrationDataType } from './auth-api'
 
-export const authReducer = (state: any, action: any): any => {
+const InitialState: InitialStateType = {
+  isLoggedIn: false,
+  isRegisteredIn: false,
+}
+
+export const authReducer = (
+  state: InitialStateType = InitialState,
+  action: any
+): InitialStateType => {
   switch (action.type) {
     case 'login/SET-IS-LOGGED-IN':
       return { ...state, isLoggedIn: action.value }
+    case 'login/SET-IS-REGISTERED-IN':
+      return { ...state, isRegisteredIn: action.value }
     default:
       return state
   }
 }
+
+export const setIsRegisteredInAC = (value: boolean) =>
+  ({ type: 'login/SET-IS-REGISTERED-IN', value } as const)
 
 export const registrationTC =
   (data: RegistrationDataType): AppThunkType =>
@@ -19,5 +32,20 @@ export const registrationTC =
       password: data.password,
     }
 
-    registrationAPI.registration(dataForServer).then(responce => console.log(responce))
+    registrationAPI
+      .registration(dataForServer)
+      .then(response => {
+        console.log(response)
+        dispatch(setIsRegisteredInAC(true))
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
+
+//TYPES
+export type RegisteredActionType = ReturnType<typeof setIsRegisteredInAC>
+type InitialStateType = {
+  isRegisteredIn: boolean
+  isLoggedIn: boolean
+}
