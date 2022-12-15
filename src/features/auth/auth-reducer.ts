5b5e9ec5c0ee4a13a80adee852adf1b8b65a7f59
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-15 22:08:39 +0100

    feat: add isNewPasswordSet field to InitialState
    add setIsNewPasswordSetAC

--- src/features/auth/auth-reducer.ts
+++ src/features/auth/auth-reducer.ts
@@ -1,14 +1,15 @@
 import axios, { AxiosError } from 'axios'
 
-import { setAppStatusAC, setUserDataAC } from '../../app/app-reducer'
-import { AppThunkDispatch, AppRootState, AppThunkType } from '../../app/store'
+import { setAppFeedbackAC, setAppStatusAC, setUserDataAC } from '../../app/app-reducer'
+import { AppRootState, AppThunkDispatch, AppThunkType } from '../../app/store'
 import { errorUtils } from '../../common/utils/error-utils'
 
-import { authAPI, ForgotPasswordType, LoginPayloadType, registrationAPI } from './auth-api'
+import { authAPI, ForgotPasswordType, LoginPayloadType } from './auth-api'
 
 const InitialState: InitialStateType = {
   isLoggedIn: false,
   isRegisteredIn: false,
+  isNewPasswordSet: false,
 }
 
 export const authReducer = (
@@ -20,6 +21,8 @@ export const authReducer = (
       return { ...state, isLoggedIn: action.isLoggedIn }
     case 'login/SET-IS-REGISTERED-IN':
       return { ...state, isRegisteredIn: action.value }
+    case 'login/IS-NEW-PASSWORD-SET':
+      return { ...state, isNewPasswordSet: action.value }
     default:
       return state
   }
@@ -33,6 +36,8 @@ export const setIsLoggedInAC = (isLoggedIn: boolean) =>
 
 export const setIsRegisteredInAC = (value: boolean) =>
   ({ type: 'login/SET-IS-REGISTERED-IN', value } as const)
+export const setIsNewPasswordSetAC = (value: boolean) =>
+  ({ type: 'login/IS-NEW-PASSWORD-SET', value } as const)
 
 export const loginTC =
   (data: LoginPayloadType): AppThunkType =>
@@ -94,42 +99,92 @@ export const logoutTC = () => (dispatch: AppThunkDispatch) => {
 export const registrationTC =
   (data: ValuesFromRegistrationType): AppThunkType =>
   (dispatch: AppThunkDispatch) => {
+    dispatch(setAppStatusAC('loading'))
     let dataForServer = {
       email: data.email,
       password: data.password,
     }
 
-    registrationAPI
-      .registration(dataForServer)
+    // registrationAPI
+    //   .registration(dataForServer)
+    axios
+      .post('https://neko-back.herokuapp.com/2.0/auth/register', dataForServer)
       .then(response => {
         console.log(response)
         dispatch(setIsRegisteredInAC(true))
+        dispatch(setAppStatusAC('succeeded'))
+        dispatch(setAppFeedbackAC('Congratulations you are registered!'))
       })
       .catch(error => {
-        console.log(error)
+        const err = error as Error | AxiosError<{ error: string }>
+
+        errorUtils(err, dispatch)
+        dispatch(setAppStatusAC('failed'))
       })
   }
 
 export const sendEmailToSetNewPasswordTC =
   (data: dataFromForgotPasswordType): AppThunkType =>
   (dispatch: AppThunkDispatch) => {
+    dispatch(setAppStatusAC('loading'))
     let dataToChangePassword: ForgotPasswordType = {
       email: data.email,
       from: 'test-front-admin <seo.spb2015@yandex.ru>',
       message: `<div style="background-color: lime; padding: 15px">
 password recovery link: 
-<a href='http://localhost:3000/#/forgot-password/$token$'>
+<a href='http://localhost:3000/#/create-password/$token$'>
 link</a>
 </div>`,
     }
 
     axios
       .post('https://neko-back.herokuapp.com/2.0/auth/forgot', dataToChangePassword)
-      .then(response => console.log(response))
-      .catch(error => console.log(error))
+      .then(response => {
+        dispatch(setAppStatusAC('succeeded'))
+        dispatch(setIsRegisteredInAC(false))
+        dispatch(setIsNewPasswordSetAC(false))
+        dispatch(setAppFeedbackAC(response.data.info))
+        console.log(response)
+      })
+      .catch(error => {
+        const err = error as Error | AxiosError<{ error: string }>
+
+        errorUtils(err, dispatch)
+        dispatch(setAppStatusAC('failed'))
+
+        console.log(error)
+      })
+  }
+
+export const setNewPasswordTC =
+  (data: setNewPasswordType): AppThunkType =>
+  (dispatch: AppThunkDispatch) => {
+    dispatch(setAppStatusAC('loading'))
+    // registrationAPI
+    //   .setNewPassword(data)
+    axios
+      .post('https://neko-back.herokuapp.com/2.0/auth/set-new-password', data)
+      .then(response => {
+        dispatch(setIsNewPasswordSetAC(true))
+        dispatch(setIsRegisteredInAC(false))
+        dispatch(setAppStatusAC('succeeded'))
+        dispatch(setAppFeedbackAC(response.data.info))
+      })
+      .catch(error => {
+        const err = error as Error | AxiosError<{ error: string }>
+
+        errorUtils(err, dispatch)
+        dispatch(setAppStatusAC('failed'))
+      })
   }
 
 //TYPES
+
+export type setNewPasswordType = {
+  password: string
+  resetPasswordToken: string
+}
+
 type dataFromForgotPasswordType = {
   email: string
 }
@@ -144,8 +199,13 @@ export type ValuesFromRegistrationType = {
 type InitialStateType = {
   isRegisteredIn: boolean
   isLoggedIn: boolean
+  isNewPasswordSet: boolean
 }
 
-export type authActionType = setIsLoggedInACType | setIsRegisteredInACType
+export type authActionType =
+  | setIsLoggedInACType
+  | setIsRegisteredInACType
+  | setIsNewPasswordSetACType
 type setIsLoggedInACType = ReturnType<typeof setIsLoggedInAC>
 type setIsRegisteredInACType = ReturnType<typeof setIsRegisteredInAC>
+type setIsNewPasswordSetACType = ReturnType<typeof setIsNewPasswordSetAC>
