293348d3f9f92c3343742d5e58ca683a054f486c
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-15 22:13:11 +0100

    refactor: add new route with token

--- src/common/components/Routing/Routes.tsx
+++ src/common/components/Routing/Routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 import { CheckEmail } from '../../../features/auth/ForgotPassword/CheckEmail'
 import { CreateNewPassword } from '../../../features/auth/ForgotPassword/CreateNewPassword'
@@ -8,7 +8,6 @@ import { ForgotPassword } from '../../../features/auth/ForgotPassword/ForgotPass
 import { Login } from '../../../features/auth/Login/Login'
 import { Register } from '../../../features/auth/Register/Register'
 import { Profile } from '../../../features/profile/Profile'
-import { NotFound } from '../PageNotFound/NotFound'
 
 export const PATH = {
   MAIN: '/',
@@ -18,6 +17,7 @@ export const PATH = {
   FORGOT_PASSWORD: '/forgot-password',
   CHECK_EMAIL: '/check-email',
   CREATE_PASSWORD: '/create-password',
+  CREATE_PASSWORD_TOKEN: '/create-password/:token',
   NOT_FOUND: '/404',
 }
 
@@ -31,8 +31,9 @@ export const AppRoutes = () => {
       <Route path={PATH.FORGOT_PASSWORD} element={<ForgotPassword />} />
       <Route path={PATH.CHECK_EMAIL} element={<CheckEmail />} />
       <Route path={PATH.CREATE_PASSWORD} element={<CreateNewPassword />} />
-      <Route path={PATH.NOT_FOUND} element={<NotFound />} />
-      <Route path={'*'} element={<Navigate to="/404" />} />
+      <Route path={PATH.CREATE_PASSWORD_TOKEN} element={<CreateNewPassword />} />
+      {/*<Route path={PATH.NOT_FOUND} element={<NotFound />} />*/}
+      {/*<Route path={'*'} element={<Navigate to="/404" />} />*/}
     </Routes>
   )
 }
