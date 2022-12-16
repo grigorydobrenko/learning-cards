1454fd5fdd14121dfa626c2457e708ee0ddd9d1e
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-16 14:29:57 +0100

    fix: fix ESLint

--- src/features/auth/Login/Login.tsx
+++ src/features/auth/Login/Login.tsx
@@ -16,7 +16,7 @@ import {
 import FormControl from '@mui/material/FormControl'
 import FormGroup from '@mui/material/FormGroup'
 import { useFormik } from 'formik'
-import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { PATH } from '../../../common/components/Routing/Routes'
 import { useAppDispatch, useAppSelector } from '../../../common/hooks/customHooks'
@@ -76,6 +76,7 @@ export const Login = () => {
       formik.resetForm()
     },
   })
+
   //nya-admin@nya.nya
   //1qazxcvBG
   useEffect(() => {
