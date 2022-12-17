09ebe1130dc1fc45687cf63955a89f483be96253
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-17 20:20:54 +0100

    refactor: change routes with token and fix rout 404

--- src/common/components/Routing/Routes.tsx
+++ src/common/components/Routing/Routes.tsx
@@ -8,6 +8,7 @@ import { ForgotPassword } from '../../../features/auth/ForgotPassword/ForgotPass
 import { Login } from '../../../features/auth/Login/Login'
 import { Register } from '../../../features/auth/Register/Register'
 import { Profile } from '../../../features/profile/Profile'
+import { NotFound } from '../PageNotFound/NotFound'
 
 export const PATH = {
   MAIN: '/',
@@ -25,12 +26,15 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route path={PATH.MAIN} element={<Profile />} />
+      <Route path={PATH.PROFILE} element={<Profile />} />
       <Route path={PATH.REGISTER} element={<Register />} />
       <Route path={PATH.LOGIN} element={<Login />} />
       <Route path={PATH.FORGOT_PASSWORD} element={<ForgotPassword />} />
       <Route path={PATH.CHECK_EMAIL} element={<CheckEmail />} />
-      <Route path={PATH.CREATE_PASSWORD} element={<CreateNewPassword />} />
-      <Route path={PATH.CREATE_PASSWORD_TOKEN} element={<CreateNewPassword />} />
+      <Route path={PATH.CREATE_PASSWORD} element={<CreateNewPassword />}>
+        <Route path={PATH.CREATE_PASSWORD_TOKEN} element={<CreateNewPassword />} />
+      </Route>
+      <Route path={'*'} element={<NotFound />} />
       {/*<Route path={PATH.NOT_FOUND} element={<NotFound />} />*/}
       {/*<Route path={'*'} element={<Navigate to="/404" />} />*/}
     </Routes>
