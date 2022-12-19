6629ba2ff18bc780d224a44287d802fdb3722bcb
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-19 22:09:09 +0100

    add: add new icons and test

--- src/features/packs/TableForPacks.tsx
+++ src/features/packs/TableForPacks.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { DeleteOutlined, EditOutlined, StepForwardOutlined } from '@ant-design/icons'
 import { Table } from 'antd'
 
 const columns = [
@@ -32,6 +33,30 @@ const columns = [
   },
 ]
 
+const TeachHandler = () => {
+  console.log('Teach')
+}
+const EditHandler = () => {
+  console.log('Edit')
+}
+const DeleteHandler = () => {
+  console.log('Delete')
+}
+
+const myActions = [
+  // eslint-disable-next-line react/jsx-key
+  <StepForwardOutlined style={{ fontSize: '20px', margin: '0 5px' }} onClick={TeachHandler} />,
+  // eslint-disable-next-line react/jsx-key
+  <EditOutlined style={{ fontSize: '20px', margin: '0 5px' }} onClick={EditHandler} />,
+  // eslint-disable-next-line react/jsx-key
+  <DeleteOutlined style={{ fontSize: '20px', marginLeft: '5px' }} onClick={DeleteHandler} />,
+]
+
+const notMyActions = [
+  // eslint-disable-next-line react/jsx-key
+  <StepForwardOutlined style={{ fontSize: '20px', margin: '0 5px' }} onClick={TeachHandler} />,
+]
+
 const dataSource = [
   {
     key: '1',
@@ -39,6 +64,7 @@ const dataSource = [
     cards: 32,
     lastUpdated: '2022.08.23',
     createdBy: 'Vasil Bereza',
+    actions: notMyActions,
   },
   {
     key: '2',
@@ -46,6 +72,7 @@ const dataSource = [
     cards: 12,
     lastUpdated: '2022.11.10',
     createdBy: 'Ivan Petrov',
+    actions: myActions,
   },
   {
     key: '3',
@@ -53,6 +80,7 @@ const dataSource = [
     cards: 1,
     lastUpdated: '2022.04.13',
     createdBy: 'Muhojouk',
+    actions: notMyActions,
   },
   {
     key: '4',
@@ -60,6 +88,7 @@ const dataSource = [
     cards: 21,
     lastUpdated: '2022.10.11',
     createdBy: 'Capitan O4evidnost',
+    actions: myActions,
   },
   {
     key: '5',
@@ -67,6 +96,7 @@ const dataSource = [
     cards: 15,
     lastUpdated: '2021.08.23',
     createdBy: 'Prodam garag',
+    actions: notMyActions,
   },
   {
     key: '6',
@@ -74,13 +104,18 @@ const dataSource = [
     cards: 8,
     lastUpdated: '2021.01.10',
     createdBy: 'Vse tlen',
+    actions: myActions,
   },
 ]
 
 export const TableForPacks = () => {
   return (
     <div>
-      <Table dataSource={dataSource} columns={columns} />
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        pagination={{ defaultPageSize: 5, showSizeChanger: true, pageSizeOptions: [5, 10, 15, 20] }}
+      />
     </div>
   )
 }
