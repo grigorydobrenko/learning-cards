4b1dd33fe214f7ca1afa04d3427053eeca72208d
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-19 20:05:18 +0100

    feat: create component Packs.tsx with TableForPacks.tsx

--- src/features/packs/Packs.tsx
+++ src/features/packs/Packs.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
 
+import { Button, Col, Divider, Row } from 'antd'
+import Title from 'antd/lib/typography/Title'
+
+import { Navbar } from './Navbar/Navbar'
+import styles from './Packs.module.css'
+import { TableForPacks } from './TableForPacks'
+
 export const Packs = () => {
-  return <div></div>
+  const addNewPackHandler = () => {}
+
+  return (
+    <div className={styles.mainContainer}>
+      <Divider />
+      <div className={styles.head}>
+        <Title level={2}>Packs list</Title>
+        <Button onClick={addNewPackHandler}> Add new pack</Button>
+      </div>
+      <Navbar />
+      <div>
+        <Row>
+          <Col xs={24}>
+            <TableForPacks />
+          </Col>
+        </Row>
+      </div>
+    </div>
+  )
 }
