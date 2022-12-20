a594c872cc1bb092f08475e4347959cec9c961a2
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-20 21:10:30 +0100

    refactor: get data from store and render it

--- src/features/packs/TableForPacks.tsx
+++ src/features/packs/TableForPacks.tsx
@@ -3,35 +3,10 @@ import React from 'react'
 import { DeleteOutlined, EditOutlined, StepForwardOutlined } from '@ant-design/icons'
 import { Table } from 'antd'
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text: string) => <a>{text}</a>,
-  },
-  {
-    title: 'Cards',
-    dataIndex: 'cards',
-    key: 'cards',
-  },
-  {
-    title: 'Last Updated',
-    dataIndex: 'lastUpdated',
-    key: 'lastUpdated',
-    sorter: (a: any, b: any) => a.lastUpdated - b.lastUpdated,
-  },
-  {
-    title: 'Created by',
-    dataIndex: 'createdBy',
-    key: 'createdBy',
-  },
-  {
-    title: 'Actions',
-    dataIndex: 'actions',
-    key: 'actions',
-  },
-]
+import { useAppSelector } from '../../common/hooks/customHooks'
+import { packsSelector } from '../../common/selectors'
+
+import { cardPacksType } from './packs-api'
 
 const TeachHandler = () => {
   console.log('Teach')
@@ -43,79 +18,74 @@ const DeleteHandler = () => {
   console.log('Delete')
 }
 
-const myActions = [
-  // eslint-disable-next-line react/jsx-key
-  <StepForwardOutlined style={{ fontSize: '20px', margin: '0 5px' }} onClick={TeachHandler} />,
-  // eslint-disable-next-line react/jsx-key
-  <EditOutlined style={{ fontSize: '20px', margin: '0 5px' }} onClick={EditHandler} />,
-  // eslint-disable-next-line react/jsx-key
-  <DeleteOutlined style={{ fontSize: '20px', marginLeft: '5px' }} onClick={DeleteHandler} />,
-]
-
-const notMyActions = [
-  // eslint-disable-next-line react/jsx-key
-  <StepForwardOutlined style={{ fontSize: '20px', margin: '0 5px' }} onClick={TeachHandler} />,
-]
-
-const dataSource = [
-  {
-    key: '1',
-    name: 'Study cards',
-    cards: 32,
-    lastUpdated: '2022.08.23',
-    createdBy: 'Vasil Bereza',
-    actions: notMyActions,
-  },
-  {
-    key: '2',
-    name: 'Learned cards',
-    cards: 12,
-    lastUpdated: '2022.11.10',
-    createdBy: 'Ivan Petrov',
-    actions: myActions,
-  },
-  {
-    key: '3',
-    name: 'First cards',
-    cards: 1,
-    lastUpdated: '2022.04.13',
-    createdBy: 'Muhojouk',
-    actions: notMyActions,
-  },
-  {
-    key: '4',
-    name: 'Second cards',
-    cards: 21,
-    lastUpdated: '2022.10.11',
-    createdBy: 'Capitan O4evidnost',
-    actions: myActions,
-  },
-  {
-    key: '5',
-    name: 'Best cards',
-    cards: 15,
-    lastUpdated: '2021.08.23',
-    createdBy: 'Prodam garag',
-    actions: notMyActions,
-  },
-  {
-    key: '6',
-    name: 'Finish cards',
-    cards: 8,
-    lastUpdated: '2021.01.10',
-    createdBy: 'Vse tlen',
-    actions: myActions,
-  },
-]
-
 export const TableForPacks = () => {
+  const myActions = [
+    // eslint-disable-next-line react/jsx-key
+    <StepForwardOutlined style={{ fontSize: '20px', margin: '0 5px' }} onClick={TeachHandler} />,
+    // eslint-disable-next-line react/jsx-key
+    <EditOutlined style={{ fontSize: '20px', margin: '0 5px' }} onClick={EditHandler} />,
+    // eslint-disable-next-line react/jsx-key
+    <DeleteOutlined style={{ fontSize: '20px', marginLeft: '5px' }} onClick={DeleteHandler} />,
+  ]
+  const notMyActions = [
+    // eslint-disable-next-line react/jsx-key
+    <StepForwardOutlined style={{ fontSize: '20px', margin: '0 5px' }} onClick={TeachHandler} />,
+  ]
+  const cardPacks = useAppSelector(packsSelector.cardPacks).map((pack: cardPacksType) => {
+    return {
+      key: pack._id,
+      name: pack.name,
+      cards: pack.cardsCount,
+      lastUpdated: pack.updated,
+      createdBy: pack.user_name,
+    }
+  })
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text: string) => <a>{text}</a>,
+    },
+    {
+      title: 'Cards',
+      dataIndex: 'cards',
+      key: 'cards',
+    },
+    {
+      title: 'Last Updated',
+      dataIndex: 'lastUpdated',
+      key: 'lastUpdated',
+      sorter: (a: any, b: any) => a.lastUpdated - b.lastUpdated,
+    },
+    {
+      title: 'Created by',
+      dataIndex: 'createdBy',
+      key: 'createdBy',
+    },
+    {
+      title: 'Actions',
+      key: 'actions',
+    },
+  ]
+
+  console.log(cardPacks)
+
   return (
     <div>
       <Table
-        dataSource={dataSource}
+        dataSource={cardPacks}
         columns={columns}
         pagination={{ defaultPageSize: 5, showSizeChanger: true, pageSizeOptions: [5, 10, 15, 20] }}
       />
     </div>
   )
+}
+
+type TablePacksType = {
+  key: string
+  name: string
+  cards: number
+  lastUpdated: string
+  createdBy: string
 }
