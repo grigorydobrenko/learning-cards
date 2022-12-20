821b72b6ced80247e0f5c2a8eb61807547943ae0
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-20 22:32:53 +0100

    add: add setMyPacksDataAC and add 'my packs' to store

--- src/features/packs/packs-reducer.ts
+++ src/features/packs/packs-reducer.ts
@@ -4,7 +4,7 @@ import { setAppStatusAC } from '../../app/app-reducer'
 import { AppThunkType } from '../../app/store'
 import { errorUtils } from '../../common/utils/error-utils'
 
-import { cardPacksType, PackResponseType, PacksRequestType, packsTableAPI } from './packs-api'
+import { CardPacksType, PackResponseType, PacksRequestType, packsTableAPI } from './packs-api'
 
 const InitialState: InitialStateType = {
   cardPacks: [],
@@ -25,6 +25,8 @@ export const packsReducer = (
   switch (action.type) {
     case 'packs/SET-PACKS':
       return { ...state, ...action.data }
+    case 'packs/SET-MY-PACKS':
+      return { ...state, cardPacks: action.data }
     default:
       return state
   }
@@ -33,6 +35,8 @@ export const packsReducer = (
 //ACTIONS========================================
 export const setPacksDataAC = (data: PackResponseType) =>
   ({ type: 'packs/SET-PACKS', data } as const)
+export const setMyPacksDataAC = (data: Array<CardPacksType>) =>
+  ({ type: 'packs/SET-MY-PACKS', data } as const)
 
 //THUNKS =========================================
 
@@ -63,10 +67,37 @@ export const getPacksTC = (): AppThunkType => async dispatch => {
   }
 }
 
+export const addNewPackTC = (): AppThunkType => async dispatch => {
+  dispatch(setAppStatusAC('loading'))
+  // const requestData: PacksRequestType = {
+  //   packName: 'english',
+  //   min: 3,
+  //   max: 22,
+  //   sortPacks: '0updated',
+  //   page: 1,
+  //   pageCount: 10,
+  //   user_id: '',
+  //   block: false,
+  // }
+
+  try {
+    const response = await packsTableAPI.createNewPack()
+
+    dispatch(setPacksDataAC(response.data))
+
+    dispatch(setAppStatusAC('succeeded'))
+  } catch (e) {
+    const err = e as Error | AxiosError<{ error: string }>
+
+    errorUtils(err, dispatch)
+    dispatch(setAppStatusAC('failed'))
+  }
+}
+
 //TYPES ==========================================
 
 type InitialStateType = {
-  cardPacks: Array<cardPacksType>
+  cardPacks: Array<CardPacksType>
   settings: Partial<PacksRequestType>
   cardPacksTotalCount: number | null
   maxCardsCount: number | null
@@ -77,5 +108,6 @@ type InitialStateType = {
   tokenDeathTime: number | null
 }
 
-export type packsReducerActionType = setPacksDataACType
+export type packsReducerActionType = setPacksDataACType | setMyPacksDataACType
 export type setPacksDataACType = ReturnType<typeof setPacksDataAC>
+export type setMyPacksDataACType = ReturnType<typeof setMyPacksDataAC>
