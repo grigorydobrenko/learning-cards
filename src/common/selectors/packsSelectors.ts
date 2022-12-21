39fe603f1c0376524a5ad3b356888eb2baaae214
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-21 17:20:26 +0100

    refactor: add new selectors

--- src/common/selectors/packsSelectors.ts
+++ src/common/selectors/packsSelectors.ts
@@ -1,3 +1,5 @@
 import { AppRootState } from '../../app/store'
 
 export const cardPacks = (state: AppRootState) => state.packs.cardPacks
+export const minCountCardsInPacks = (state: AppRootState) => state.packs.minCountCardsInPacks
+export const maxCountCardsInPacks = (state: AppRootState) => state.packs.maxCountCardsInPacks
