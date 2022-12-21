4d3398fbaaca08e17da3000d96e5416ca013ff80
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-21 21:42:21 +0100

    add: add new selectors

--- src/common/selectors/packsSelectors.ts
+++ src/common/selectors/packsSelectors.ts
@@ -1,9 +1,9 @@
 import { AppRootState } from '../../app/store'
 
 export const cardPacks = (state: AppRootState) => state.packs.cardPacks
-export const minCountCardsInPacks = (state: AppRootState) => state.packs.minCountCardsInPacks
-export const maxCountCardsInPacks = (state: AppRootState) => state.packs.maxCountCardsInPacks
+export const min = (state: AppRootState) => state.packs.min
+export const max = (state: AppRootState) => state.packs.max
 export const isMyPacks = (state: AppRootState) => state.packs.isMyPacks
-export const search = (state: AppRootState) => state.packs.search
+export const search = (state: AppRootState) => state.packs.packName
 export const sort = (state: AppRootState) => state.packs.sort
 export const pageCount = (state: AppRootState) => state.packs.pageCount
