73391cc331ff0188b05cfea569f33a855cbc18d5
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-23 00:33:55 +0100

    add: add pagination

--- src/features/packs/TableForPacks.tsx
+++ src/features/packs/TableForPacks.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { DeleteOutlined, EditOutlined, StepForwardOutlined } from '@ant-design/icons'
 import { Col, Row, Table } from 'antd'
 import { Link } from 'react-router-dom'
 
-import { useAppSelector } from '../../common/hooks/customHooks'
+import { useAppDispatch, useAppSelector } from '../../common/hooks/customHooks'
 import { appSelector, packsSelector } from '../../common/selectors'
 
 import { CardPacksType } from './packs-api'
+import { getPacksTC, setPagePacksCountAC } from './packs-reducer'
 
 export const TableForPacks = () => {
+  const dispatch = useAppDispatch()
   const userData = useAppSelector(appSelector.user)
+  const { pageCount, page } = useAppSelector(state => state.packs)
 
   const TeachHandler = () => {
     console.log('Teach')
@@ -19,6 +22,7 @@ export const TableForPacks = () => {
     console.log('Edit')
   }
   const DeleteHandler = () => {
+    // dispatch(deletePackTC(e))
     console.log('Delete')
   }
   const getDate = (dateString: string) => {
@@ -34,7 +38,11 @@ export const TableForPacks = () => {
       // eslint-disable-next-line react/jsx-key
       <EditOutlined style={{ fontSize: '15px', margin: '0 5px' }} onClick={EditHandler} />,
       // eslint-disable-next-line react/jsx-key
-      <DeleteOutlined style={{ fontSize: '15px', marginLeft: '5px' }} onClick={DeleteHandler} />,
+      <DeleteOutlined
+        key={pack._id}
+        style={{ fontSize: '15px', marginLeft: '5px' }}
+        onClick={DeleteHandler}
+      />,
     ]
 
     const notMyActions = [
@@ -80,7 +88,6 @@ export const TableForPacks = () => {
       title: 'Cards',
       dataIndex: 'cards',
       key: 'key',
-      sorter: (a: any, b: any) => a.cards - b.cards,
     },
     {
       title: 'Last Updated',
@@ -100,7 +107,19 @@ export const TableForPacks = () => {
       render: (myActions: any) => myActions,
     },
   ]
+  const pagination = {
+    defaultPageSize: pageCount,
+    showSizeChanger: true,
+    total: page,
+    pageSizeOptions: [2, 5, 7, 10, 15, 20],
+    onChange: (page: number, pageSize: number) => {
+      dispatch(setPagePacksCountAC(pageSize, page))
+    },
+  }
 
+  useEffect(() => {
+    dispatch(getPacksTC())
+  }, [pageCount, page])
   console.log(cardPacks)
 
   return (
@@ -111,11 +130,7 @@ export const TableForPacks = () => {
             dataSource={cardPacks}
             tableLayout={'fixed'}
             columns={columns}
-            pagination={{
-              defaultPageSize: 5,
-              showSizeChanger: true,
-              pageSizeOptions: [5, 10, 15, 20],
-            }}
+            pagination={pagination}
           />
         </Col>
       </Row>
