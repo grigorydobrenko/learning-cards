6c1805608d02241bd4ffc7d1c19defe31c26ee8b
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-26 19:28:53 +0100

    refactor: refactor Search.tsx

--- src/features/packs/Navbar/Search/Search.tsx
+++ src/features/packs/Navbar/Search/Search.tsx
@@ -1,11 +1,11 @@
 import React, { ChangeEvent, useEffect, useState } from 'react'
 
-import Search from 'antd/lib/input/Search'
+import { Input } from 'antd'
 
 import { useAppDispatch, useAppSelector } from '../../../../common/hooks/customHooks'
 import { packsSelector } from '../../../../common/selectors'
 import { useDebounce } from '../../../../common/utils/debounce'
-import { getPacksTC, setSearchDataAC } from '../../packs-reducer'
+import { setSearchDataAC } from '../../packs-reducer'
 
 export const SearchComponent = () => {
   const dispatch = useAppDispatch()
@@ -16,38 +16,25 @@ export const SearchComponent = () => {
   const debouncedSearchValue = useDebounce(searchValue, 700)
   const onSearchHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.currentTarget.value)
-    console.log('onSearchHandler: ', e.currentTarget.value)
   }
 
   useEffect(() => {
     if (searchValue) {
       dispatch(setSearchDataAC(searchValue))
-    } else if (searchValue === '') {
-      dispatch(setSearchDataAC(null))
-      dispatch(getPacksTC())
     }
-
-    console.log('useEffect [debouncedSearchValue] in SearchComponent worked')
   }, [debouncedSearchValue])
 
   useEffect(() => {
-    if (packName) {
-      dispatch(getPacksTC())
-    } else {
+    if (packName === null) {
       setSearchValue(null)
     }
-
-    //setSearchValue(packName)
-    console.log('useEffect [packName] in SearchComponent worked')
   }, [packName])
 
   return (
     <>
-      <Search
-        //placeholder="input search text"
+      <Input
         value={searchValue ? searchValue : ''}
-        //placeholder={searchValue ? searchValue : 'input search text'}
-        // value={searchValue ? searchValue : ''}
+        placeholder={searchValue ? searchValue : 'input search text'}
         allowClear
         onChange={onSearchHandler}
         style={{ width: 250 }}
