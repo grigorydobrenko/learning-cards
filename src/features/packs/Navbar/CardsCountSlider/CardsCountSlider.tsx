49667291278f9b1106b8a299e8bcf17dcacb8885
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-26 19:27:15 +0100

    refactor: refactor CardsCountSlider.tsx

--- src/features/packs/Navbar/CardsCountSlider/CardsCountSlider.tsx
+++ src/features/packs/Navbar/CardsCountSlider/CardsCountSlider.tsx
@@ -5,15 +5,13 @@ import { Col, Input, Slider } from 'antd'
 import { useAppDispatch, useAppSelector } from '../../../../common/hooks/customHooks'
 import { useDebounce } from '../../../../common/hooks/useDebounce'
 import { packsSelector } from '../../../../common/selectors'
-import { getPacksTC, setMaxCardsCountAC, setMinCardsCountAC } from '../../packs-reducer'
+import { setMaxCardsCountAC, setMinCardsCountAC } from '../../packs-reducer'
 
 export const CardsCountSlider = () => {
   const dispatch = useAppDispatch()
 
   const min = useAppSelector(packsSelector.min)
   const max = useAppSelector(packsSelector.max)
-  // const minCardsCount = useAppSelector(packsSelector.minCardsCount)
-  // const maxCardsCount = useAppSelector(packsSelector.maxCardsCount)
 
   const [minCards, setMinCards] = useState<number>(min)
   const [maxCards, setMaxCards] = useState<number>(max)
@@ -23,28 +21,20 @@ export const CardsCountSlider = () => {
 
   useEffect(() => {
     dispatch(setMinCardsCountAC(minCards))
-
-    console.log('useEffect -> setMinCardsCountAC: worked', minCards)
   }, [debouncedMinCardsCount])
 
   useEffect(() => {
     dispatch(setMaxCardsCountAC(maxCards))
-
-    console.log('useEffect -> setMaxCardsCountAC: worked', maxCards)
   }, [debouncedMaxCardsCount])
 
   useEffect(() => {
     setMinCards(min)
     setMaxCards(max)
-    dispatch(getPacksTC())
-
-    console.log('useEffect [min, max] in CardsCountSlider worked')
   }, [min, max])
 
   const onChangeCardsCountSlider = ([minCards, maxCards]: Array<number>) => {
     setMinCards(minCards)
     setMaxCards(maxCards)
-    console.log([minCards, maxCards])
   }
 
   return (
