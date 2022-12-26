0fac2acbb6a37e6f204657b71fdb4c499bd719fe
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-26 19:27:33 +0100

    refactor: refactor Packs.tsx

--- src/features/packs/Packs.tsx
+++ src/features/packs/Packs.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Button, CircularProgress } from '@mui/material'
 import { Col, Divider, Row } from 'antd'
@@ -6,20 +6,28 @@ import Title from 'antd/lib/typography/Title'
 
 import { AddPackModal } from '../../common/components/Modals/PackModals/AddPackModal'
 import { useAppDispatch, useAppSelector } from '../../common/hooks/customHooks'
-import { appSelector } from '../../common/selectors'
+import { appSelector, packsSelector } from '../../common/selectors'
 
 import { Navbar } from './Navbar/Navbar'
-import { addNewPackTC } from './packs-reducer'
+import { addNewPackTC, getPacksTC } from './packs-reducer'
 import styles from './Packs.module.css'
 import { TableForPacks } from './TableForPacks/TableForPacks'
 
 export const Packs = () => {
   const dispatch = useAppDispatch()
   const status = useAppSelector(appSelector.status)
+  const min = useAppSelector(packsSelector.min)
+  const max = useAppSelector(packsSelector.max)
+  const packName = useAppSelector(packsSelector.packName)
+  const user_id = useAppSelector(packsSelector.user_id)
 
   const addNewPackHandler = (name: string) => {
     dispatch(addNewPackTC(name))
   }
+
+  useEffect(() => {
+    dispatch(getPacksTC())
+  }, [min, max, packName, user_id])
 
   return (
     <div className={styles.mainContainer}>
