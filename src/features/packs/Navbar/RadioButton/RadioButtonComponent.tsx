730112ec935ad68ca51f30bd0c4446e7fa0603cb
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-26 14:48:48 +0100

    refactor: change RadioButtonComponent.tsx

--- src/features/packs/Navbar/RadioButton/RadioButtonComponent.tsx
+++ src/features/packs/Navbar/RadioButton/RadioButtonComponent.tsx
@@ -3,44 +3,53 @@ import React, { useState } from 'react'
 import { Radio, RadioChangeEvent } from 'antd'
 
 import { useAppDispatch, useAppSelector } from '../../../../common/hooks/customHooks'
-import { appSelector } from '../../../../common/selectors'
+import { appSelector, packsSelector } from '../../../../common/selectors'
 import { getPacksTC, setUserIdAC } from '../../packs-reducer'
 
 export const RadioButtonComponent = () => {
   const dispatch = useAppDispatch()
 
   const userData = useAppSelector(appSelector.user)
-  //const user_id = useAppSelector(packsSelector.user_id)
+  const isMyPacks = useAppSelector(packsSelector.isMyPacks)
 
-  const [choosePacks, setChoosePacks] = useState<string>('all')
-  const onChangeFilterHandler = ({ target: { value } }: RadioChangeEvent) => {
-    console.log('radio checked', value)
-    setChoosePacks(value)
+  const [choosePacks, setChoosePacks] = useState<string>(isMyPacks)
+  const onChangeFilterHandler = (e: RadioChangeEvent) => {
+    console.log('radio checked', e.target.value)
+    setChoosePacks(e.target.value)
     if (choosePacks !== 'my' && userData) {
       dispatch(setUserIdAC(userData._id))
+      //dispatch(setIsMyPacksAC(true))
     } else {
       dispatch(setUserIdAC(''))
+      //dispatch(setIsMyPacksAC(false))
     }
     dispatch(getPacksTC())
   }
-  const options = [
-    { label: 'My', value: 'my' },
-    { label: 'All', value: 'all' },
-  ]
+  // const options = [
+  //   { label: 'My', value: true },
+  //   { label: 'All', value: false },
+  // ]
 
-  /* useEffect(() => {
-    dispatch(getPacksTC())
-    console.log('useEffect [user_id] in RadioButtonComponent worked')
-  }, [user_id])*/
+  // useEffect(() => {
+  //   dispatch(getPacksTC())
+  //   console.log('useEffect [user_id] in RadioButtonComponent worked')
+  // }, [isMyPacks])
 
   return (
     <>
       <Radio.Group
-        options={options}
+        // options={options}
         onChange={onChangeFilterHandler}
-        value={choosePacks}
-        optionType="button"
-      />
+        // value={choosePacks}
+        // optionType="button"
+      >
+        <Radio.Button checked={choosePacks !== 'all'} value="my">
+          My
+        </Radio.Button>
+        <Radio.Button checked={choosePacks !== 'my'} value="all">
+          All
+        </Radio.Button>
+      </Radio.Group>
     </>
   )
 }
