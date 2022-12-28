592da6f293c73456e04b375771c2d75ec47328db
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-28 19:35:01 +0100

    refactor: add load spinner for table is loading, add description when invalid search, add filter to card column, disable teach button if 0 cards

--- src/features/packs/TableForPacks/TableForPacks.tsx
+++ src/features/packs/TableForPacks/TableForPacks.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { DeleteOutlined, EditOutlined, PlaySquareOutlined, SyncOutlined } from '@ant-design/icons'
-import { Col, Row, Table } from 'antd'
+import { Col, ConfigProvider, Empty, Row, Table } from 'antd'
 import { Link } from 'react-router-dom'
 import { v4 as uuid4 } from 'uuid'
 
@@ -18,6 +18,7 @@ export const TableForPacks = () => {
   const dispatch = useAppDispatch()
   const userData = useAppSelector(appSelector.user)
   const status = useAppSelector(appSelector.status)
+  const packName = useAppSelector(packsSelector.packName)
   const TeachHandler = (id: string) => {
     console.log('Teach. id ->', id)
   }
@@ -39,7 +40,9 @@ export const TableForPacks = () => {
         key={uuid4()}
         style={{ fontSize: '15px', margin: '0 5px' }}
         onClick={() => TeachHandler(pack._id)}
+        className={pack.cardsCount === 0 ? s.disabledButton : ''}
       />,
+
       <EditPackModal
         key={uuid4()}
         editPackHandler={EditHandler}
@@ -72,6 +75,7 @@ export const TableForPacks = () => {
         key={uuid4()}
         style={{ fontSize: '15px', margin: '0 5px' }}
         onClick={() => TeachHandler(pack._id)}
+        className={pack.cardsCount === 0 ? s.disabledButton : ''}
       />,
     ]
 
@@ -93,46 +97,65 @@ export const TableForPacks = () => {
     {
       title: 'Name',
       dataIndex: 'name',
-      key: 'key',
+      key: 'name',
     },
     {
       title: 'Cards',
       dataIndex: 'cards',
-      key: 'key',
+      key: 'cards',
+
+      sorter: (a: any, b: any) => a.cards - b.cards,
     },
     {
       title: 'Last Updated',
       dataIndex: 'lastUpdated',
-      key: 'key',
+      key: 'lastUpdated',
+
       sorter: (a: any, b: any) => a.lastUpdated.localeCompare(b.lastUpdated),
     },
     {
       title: 'Created by',
       dataIndex: 'createdBy',
-      key: 'key',
+      key: 'createdBy',
+      sorter: (a: any, b: any) => (a.createdBy > b.createdBy ? 1 : -1),
     },
     {
       title: 'Actions',
       dataIndex: 'actions',
-      key: 'key',
+      key: 'actions',
+      // className: {display: 'flex', flexDirection: 'row'},
       render: (actions: any) => actions,
     },
   ]
 
   return (
-    <div className={status === 'loading' ? s.disabledButton : ''}>
+    <div className={s.mainContainer}>
       <Row>
         <Col xs={24} span={12}>
-          <Table
-            dataSource={cardPacks}
-            tableLayout={'fixed'}
-            columns={columns}
-            pagination={{
-              defaultPageSize: 5,
-              showSizeChanger: true,
-              pageSizeOptions: [5, 10, 15, 20],
-            }}
-          />
+          <ConfigProvider
+            renderEmpty={cardPacks =>
+              cardPacks ? (
+                cardPacks
+              ) : (
+                <Empty
+                  style={{ color: 'black' }}
+                  description="Колод с таким названием не нашлось :("
+                />
+              )
+            }
+          >
+            <Table
+              loading={status === 'loading' ? { spinning: true, size: 'large' } : false}
+              dataSource={cardPacks}
+              tableLayout={'fixed'}
+              columns={columns}
+              pagination={{
+                defaultPageSize: 5,
+                showSizeChanger: true,
+                pageSizeOptions: [5, 10, 15, 20],
+              }}
+            />
+          </ConfigProvider>
         </Col>
       </Row>
     </div>
