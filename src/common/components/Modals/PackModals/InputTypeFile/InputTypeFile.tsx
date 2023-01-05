19eea3af1a4bd80f1eb1c97cd657c03913671737
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-05 16:45:07 +0100

    refactor: refactor cover container

--- src/common/components/Modals/PackModals/InputTypeFile/InputTypeFile.tsx
+++ src/common/components/Modals/PackModals/InputTypeFile/InputTypeFile.tsx
@@ -1,15 +1,24 @@
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 
 import CloudUploadIcon from '@mui/icons-material/CloudUpload'
 import { IconButton } from '@mui/material'
 
-import { useAppDispatch } from '../../../hooks/customHooks'
+import { useAppDispatch, useAppSelector } from '../../../../hooks/customHooks'
+
+import s from './InputTypeFile.module.css'
 
 import defaultAva from 'assets/img/icons/default-cover.jpg'
 import { setPackDeckCoverAC } from 'features/packs/packs-reducer'
 
-export const InputTypeFile = () => {
+type InputTypeFilePropsType = {
+  id?: string
+}
+
+export const InputTypeFile = (props: InputTypeFilePropsType) => {
   const dispatch = useAppDispatch()
+  const pack = useAppSelector(
+    state => state.packs.cardPacks.filter(pack => pack._id === props.id)[0]
+  )
 
   const [ava, setAva] = useState(defaultAva)
   const [isAvaBroken, setIsAvaBroken] = useState(false)
@@ -22,6 +31,7 @@ export const InputTypeFile = () => {
         convertFileToBase64(file, (file64: string) => {
           setAva(file64)
           dispatch(setPackDeckCoverAC(file64))
+          setIsAvaBroken(false)
         })
       } else {
         console.error('Error: ', 'Файл слишком большого размера')
@@ -45,20 +55,32 @@ export const InputTypeFile = () => {
     alert('Кривая картинка')
   }
 
+  useEffect(() => {
+    if (pack && pack.deckCover) {
+      setAva(pack.deckCover)
+    }
+  }, [pack])
+
   return (
-    <div>
-      <img
-        src={isAvaBroken ? defaultAva : ava}
-        style={{ width: '100px' }}
-        onError={errorHandler}
-        alt="ava"
-      />
-      <label>
-        <input type="file" onChange={uploadHandler} style={{ display: 'none' }} />
-        <IconButton component="span">
-          <CloudUploadIcon />
-        </IconButton>
-      </label>
+    <div className={s.mainContainer}>
+      <div className={s.describeContainer}>
+        <span>Choose the cover</span>
+        <label>
+          <input type="file" onChange={uploadHandler} style={{ display: 'none' }} />
+          <IconButton component="span">
+            <CloudUploadIcon />
+          </IconButton>
+        </label>
+      </div>
+
+      <div className={s.pictureContainer}>
+        <img
+          src={isAvaBroken ? defaultAva : ava}
+          // style={{ width: '100%' }}
+          onError={errorHandler}
+          alt="ava"
+        />
+      </div>
     </div>
   )
 }
