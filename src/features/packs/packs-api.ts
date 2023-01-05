f241d01308db0ce5c6aa25a8cc0448884d59d7de
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-05 16:46:24 +0100

    refactor: refactor updatePack method in packsTableAPI

--- src/features/packs/packs-api.ts
+++ src/features/packs/packs-api.ts
@@ -26,11 +26,11 @@ export const packsTableAPI = {
     return instance.delete<DeletePackRequestType>(`/cards/pack?id=${id}`)
   },
 
-  updatePack({ cardsPack: { _id, name } }: UpdatePackNameType) {
+  updatePack({ cardsPack: { _id, name, deckCover } }: UpdatePackNameType) {
     return instance.put<'', AxiosResponse<PacksResponseType>, UpdatePackNameType>(
       // `/cards/pack?id=${id}&name=${newPackName}`
       '/cards/pack',
-      { cardsPack: { _id, name } }
+      { cardsPack: { _id, name, deckCover } }
     )
   },
 }
@@ -40,6 +40,7 @@ type UpdatePackNameType = {
   cardsPack: {
     _id: string
     name: string
+    deckCover?: string
   }
 }
 type DeletePackRequestType = {
