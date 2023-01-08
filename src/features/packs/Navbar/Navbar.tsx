df40ec51ec60472a833f135d69e89bf1226d5140
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-08 19:20:41 +0100

    refactor: refactor Navbar.tsx, use SearchParams

--- src/features/packs/Navbar/Navbar.tsx
+++ src/features/packs/Navbar/Navbar.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Col, Row } from 'antd'
+import { useSearchParams } from 'react-router-dom'
+
+import { useAppDispatch } from '../../../common/hooks/customHooks'
+import { getPacksTC } from '../packs-reducer'
 
 import { CardsCountSlider } from './CardsCountSlider/CardsCountSlider'
 import styles from './Navbar.module.css'
@@ -8,22 +12,35 @@ import { RadioButtonComponent } from './RadioButton/RadioButtonComponent'
 import { ResetSettingsComponent } from './ResetSettings/ResetSettingsComponent'
 import { SearchComponent } from './Search/SearchComponent'
 
-export const Navbar = () => {
+export const Navbar = (props: NavbarPropsType) => {
   return (
     <div className={styles.navbar}>
       <Row>
         <Col xs={6} md={{ span: 6 }}>
-          <SearchComponent />
+          <SearchComponent
+            searchParams={props.searchParams}
+            setSearchParams={props.setSearchParams}
+          />
         </Col>
         <Col xs={3} md={{ span: 3, offset: 2 }}>
-          <RadioButtonComponent />
+          <RadioButtonComponent
+            searchParams={props.searchParams}
+            setSearchParams={props.setSearchParams}
+          />
         </Col>
-        <CardsCountSlider />
-
+        <CardsCountSlider
+          searchParams={props.searchParams}
+          setSearchParams={props.setSearchParams}
+        />
         <Col xs={1} md={{ span: 1, offset: 1 }}>
-          <ResetSettingsComponent />
+          <ResetSettingsComponent setSearchParams={props.setSearchParams} />
         </Col>
       </Row>
     </div>
   )
+}
+
+type NavbarPropsType = {
+  setSearchParams: (params: any) => void
+  searchParams: any
 }
